3a98e930eb470d0ea096fb686389bbc9065b50ce
Mehedi Hassan Durjoi <m.h.durjoi@gmail.com>
2022-01-27 17:40:56 +0600

    Most Popular slider and categories page design updated

--- wp-content/themes/industrydive/category.php
+++ wp-content/themes/industrydive/category.php
@@ -3,8 +3,19 @@
     get_header();
 ?>
 
-    <main>
-        <?php get_template_part('template_part/categories_post') ?>
+    <main class="category_page">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <?php
+                        while(have_posts()) : the_post();
+                            get_template_part('template_part/post_item');
+                        endwhile;
+                    ?>
+                </div>
+            </div>
+        </div>
+        
     </main>
 
 <?php 
